585dd31f42d4a27026395213cdd7dd472dbc7b65
Monir Hossain <monir.hossain@northsouth.edu>
2021-08-04 12:21:07 +0600

    feat(post app): interface and routing added

--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {IndexComponent} from "./index/index.component";
+import {ViewComponent} from "./view/view.component";
+import {CreateComponent} from "./create/create.component";
+import {EditComponent} from "./edit/edit.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'post', redirectTo: 'post/index', pathMatch: 'full'},
+  {path: 'post/index', component: IndexComponent},
+  {path: 'post/:id/view', component: ViewComponent},
+  {path: 'post/create', component: CreateComponent},
+  {path: 'post/:id/edit', component: EditComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PostRoutingModule { }
+export class PostRoutingModule {
+}
